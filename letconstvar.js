11c1b3c28afc30f847fde7ace753a443f4934209
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2022-04-28 18:08:17 +0530

    hoist

--- letconstvar.js
+++ letconstvar.js
@@ -15,3 +15,41 @@ function fun(){
 	var name = 'Mukul Latiyan';
 }
 fun(); // undefined
+
+
+hoistedVariable = 3;
+console.log(hoistedVariable); // outputs 3 even when the variable is declared after it is initialized	
+var hoistedVariable;
+
+
+hoistedFunction();  // Outputs " Hello world! " even when the function is declared after calling
+
+function hoistedFunction(){ 
+  console.log(" Hello world! ");
+}
+
+
+
+// Hoisting takes place in the local scope as well
+function doSomething(){
+	x = 33;
+	console.log(x);
+	var x;
+  }
+  doSomething(); // Outputs 33 since the local variable “x” is hoisted inside the local scope
+
+
+var x;
+console.log(x); // Outputs "undefined" since the initialization of "x" is not hoisted
+x = 23;
+/* 
+// **Note - To avoid hoisting, 
+you can run javascript in strict mode by using “use strict” on top of the code:
+
+
+"use strict";
+x = 23; // Gives an error since 'x' is not declared
+var x; 
+
+
+*/
