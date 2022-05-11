b5b5df528e9db15629966eb430e7d3b097291eae
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2022-05-11 10:41:34 +0530

    duplicate array

---
+++ duplicateArray.js
@@ -0,0 +1,33 @@
+let arr=["Mumbai", "Delhi", "Kanpur", "Bangalore", "Mumbai", "Kanpur", "Delhi","Bangalore", "Kanpur", "Delhi" ]
+
+let uniqCount= arr.reduce((prev,cur)=>{
+    prev[cur]=(prev[cur] || 0) + 1;
+    return prev;
+},{})
+console.log(uniqCount);//{ Mumbai: 2, Delhi: 3, Kanpur: 3, Bangalore: 2 }
+
+count ={}
+
+arr.forEach(function(i){
+ count[i]=(count[i]||0)+1
+})
+console.log(count);// { Mumbai: 2, Delhi: 3, Kanpur: 3, Bangalore: 2 }
+
+
+let chars = ['A', 'B', 'A', 'C', 'B'];
+let uniqueChars = [...new Set(chars)];
+
+console.log(uniqueChars);
+
+function toUniqueArray(a){
+    var newArr = [];
+    for (var i = 0; i < a.length; i++) {
+        if (newArr.indexOf(a[i]) === -1) {
+            newArr.push(a[i]);
+        }
+    }
+  return newArr;
+}
+var colors = ["red","red","green","green","green"];
+var colorsUnique=toUniqueArray(colors); // ["red","green"]
+console.log(colorsUnique)
