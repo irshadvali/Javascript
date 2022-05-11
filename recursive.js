1696c42028a9c60dea175036625329819b644aa6
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2022-05-11 09:31:29 +0530

    reducer and currying function

--- recursive.js
+++ recursive.js
@@ -11,3 +11,22 @@ const sum = function (a){
 const sume= a => b => b ? sume(a+b): a
 console.log(sum(1)(2)(3)(4)()); //10
 console.log(sume(1)(2)(3)(4)(5)()); //10
+
+
+/*----------------------------------*/
+
+
+let arr=["Mumbai", "Delhi", "Kanpur", "Bangalore", "Mumbai", "Kanpur", "Delhi","Bangalore", "Kanpur", "Delhi" ]
+
+let uniqCount= arr.reduce((prev,cur)=>{
+    prev[cur]=(prev[cur] || 0) + 1;
+    return prev;
+},{})
+console.log(uniqCount);
+
+count ={}
+
+arr.forEach(function(i){
+ count[i]=(count[i]||0)+1
+})
+console.log(count);
