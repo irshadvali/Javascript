4409029804b6b55910240a2338412d92e8b1529f
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2022-06-01 19:02:36 +0530

    clouser

---
+++ clouserConsole.js
@@ -0,0 +1,14 @@
+console.log("hi")
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);//bar
+        console.log("outer func:  self.foo = " + self.foo);//bar
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);//undefined
+            console.log("inner func:  self.foo = " + self.foo);//bar
+        }());
+    }
+};
+myObject.func();
